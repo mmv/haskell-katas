dc3b44df25fad2eeb36ddcbfeb0a64aeefeacf4d
Miguel Ventura <miguel.ventura@gmail.com>
2013-11-20 00:03:17 +0000

    problem 9

--- eulerproject.hs
+++ eulerproject.hs
@@ -108,3 +108,17 @@ problem8 () = do
     let fullstr = (concat (lines contents))
     let asdigits = (map digitToInt fullstr)
     return (greatestProductOfXConsecutive 5 asdigits)
+
+-- problem 9
+-- pitagorian triplet (a^2+b^2=c^2) where a+b+c == 1000
+
+tripletsEqualTo n =
+    filter (\(a,b,c) -> a*a + b*b == c*c) (iter (1,2,n-3))
+    where
+        iter (a,b,c) | a < 0 || b < 0 || c < 0 = []
+                     | b >= c = iter (a+1,b-1,c)
+                     | a >= b = iter (1,n-(c-1)-1,c-1)
+                     | a <  b = (a,b,c) : iter (a+1,b-1,c)
+                     | otherwise = [(0,0,0)]
+
+problem9 () = foldl max 0 $ map (\(a,b,c) -> a*b*c) $ tripletsEqualTo 1000
